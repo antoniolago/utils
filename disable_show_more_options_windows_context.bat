3c72bd7d9aa83fb85d0100c2992b75f6b789e2c8
antoniolago <45375617+antoniolago@users.noreply.github.com>
2024-09-14 11:56:47 -0300

    Create disable_show_more_options_windows_context.bat

---
+++ disable_show_more_options_windows_context.bat
@@ -0,0 +1,10 @@
+:: disable
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+:: enable
+:: reg delete "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f​
+
+taskkill /im explorer.exe /f
+start explorer.exe
+
+PAUSE
+exit
